a14c6d541e6b6a68f81d8f15ae8deeb148fe61b5
Yarin <yarin.greenfeld@gmail.com>
2022-06-06 18:07:37 +0300

    added users table

---
+++ database/models/users.py
@@ -0,0 +1,12 @@
+
+from sqlalchemy import Column,Integer,String
+from database.models.base import Base
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(String(250), primary_key=True)
+    email = Column(String(250), nullable=False)
+    full_name = Column(String(250), nullable=False)
+    phone = Column(String(15), nullable=False)
+    course_id = Column(Integer, nullable=False)
+    language_id = Column(Integer, nullable=True)
