5df246e32044f8df57a8163c417ed2000f4ee8fe
Yarin <yarin.greenfeld@gmail.com>
2022-06-06 22:27:35 +0300

    added users accessor

--- database/users_accsesor.py
+++ database/users_accsesor.py
@@ -2,7 +2,7 @@ import datetime
 from sqlalchemy.orm import Session
 from database.accessor import Accessor
 from database.models.admins import admins
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, String
 from typing import List
 from enum import Enum
 
@@ -14,15 +14,22 @@ class User_Accessor(Accessor):
 
     per table accessors are created for the different tables.
     """
-    def __init__(self,session:Session, model ):
+    def __init__(self,session:Session, model):
         Accessor.__init__(self,session,model)   
+        self.name="users accessor"
     
-
-    #TODO: only for the users table -> should be moved to a different accsesor
-    def get_user(self,name:str):
-        return rows
+    # get user with the right username
+    def get_user(self,id:int):
+        return self.session.query(self.model).filter(self.model.id == id).first()
        
+    # add user according to the users table
+    def add_user(self,id:String,email:String,full_name:String,phone:String):
+        self.add(self.model(id = id,email = email,full_name = full_name,phone = phone))
 
-    def add_user(self,name:str,hashed_password:str):
-        self.session.add(self.model(username=name,password = hashed_password))
-        self.session.commit()
+
+    def get_row_using_email(self,email:String):
+        row = self.session.query(self.model).filter(self.model.email == email).all()
+        print(row)
+        if len(row) >1:
+            return None
+        return row[0] #HACK: should be a list with one element
