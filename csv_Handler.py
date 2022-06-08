f61cb38c6411bc5a4a03dbc7d2843ae8362d019d
Yarin <yarin.greenfeld@gmail.com>
2022-06-09 00:57:57 +0300

    added the validator class+ id

---
+++ csv_Handler.py
@@ -0,0 +1,23 @@
+
+class Validator:
+
+    def validate_israeli_id(value: str) -> bool:
+        check_digit = 0
+        weight_sum = 0
+        temp = 0
+
+        for i in range(len(value) - 1):
+            if i % 2 == 0:
+                temp = int(value[i])
+            else:
+                temp = int(value[i]) * 2
+
+            if temp > 10:
+                weight_sum += (temp % 10) + int(temp / 10)
+            else:
+                weight_sum += temp
+
+        while (weight_sum + check_digit) % 10 != 0:
+            check_digit += 1
+
+        return str(check_digit) == value[-1]
